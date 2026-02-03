18cfddf8e8dfd0ef9931c4a57cf9b9c0935e29a2
Fatih Kemal tomas <92810946+Fatihkemal1@users.noreply.github.com>
2026-02-03 14:43:45 +0300

    Add files via upload

---
+++ chapter1/basic if and input.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+
+
+	std::string isim;
+	std::cout << "sa \n";
+	std::getline(std::cin >> std::ws, isim);
+
+	if (isim == "as") {
+
+		std::cout << "baybay";
+		
+	}
+	else
+	{
+		std::cout << "ne diyon";
+	}
+}
