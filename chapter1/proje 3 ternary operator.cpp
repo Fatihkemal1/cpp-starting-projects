b2ee8f884174936bf134b8d1ec6f2b7df99fb39d
Fatih Kemal tomas <92810946+Fatihkemal1@users.noreply.github.com>
2026-02-03 16:00:02 +0300

    Add files via upload

---
+++ chapter1/proje 3 ternary operator.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+
+	double grade;
+	std::cout << "what is your grade?\n";
+	std::cin >> grade;
+
+	std::cout << (grade >= 75 ? "\nyou passed with a good grade \n\n" :
+		grade >= 60 ? "\nyou passed with a mediocre grade \n" :
+		grade >= 50 ? "\nyou passed with a bad grade\n" : "you failed \n");
+
+	return 0;
+}
